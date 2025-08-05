49dd17925a722135638f4e49730132f7c0ff6d98
FacundoMan <fmansillla@gmail.com>
2025-08-05 16:17:18 -0300

    Se agrego el movimiento, y se puso el cooldown al salto

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,29 +1,46 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
 
-    // Update is called once per frame
-   
-    public float upForce = 5f;
+
+    public float upForce = 5f, force = 5f;
     private Rigidbody rb;
+    private PlayerInput playerInput;
+    private Vector2 input;
+     private bool canJump = true;
 
     void Start()
     {
         Debug.Log("Script activo");
         rb = GetComponent<Rigidbody>();
-        GetComponent<PlayerInput>().ActivateInput(); // Forzar activación del input map
+        playerInput = GetComponent<PlayerInput>();
     }
+    void Update()
+    {
+        input = playerInput.actions["Movement"].ReadValue<Vector2>();
 
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(input.x, 0f, input.y) * force);
+    }
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && canJump)
         {
-            Debug.Log("Jump!");
+
             rb.AddForce(Vector3.up * upForce, ForceMode.Impulse);
+            StartCoroutine(JumpCooldown()); //Cooldown jump
         }
+    }
+    
+    private IEnumerator JumpCooldown()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(1f);
+        canJump = true;
     }
 }
