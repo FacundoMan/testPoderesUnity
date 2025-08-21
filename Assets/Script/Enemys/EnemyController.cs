f9c1de6e2e43ec1bd5d3bad481c1025cb08217c9
FacundoMan <fmansillla@gmail.com>
2025-08-21 19:28:58 -0300

    Se agrego musica, tambien se agrego una barra de vida prefab para los enemigos

--- Assets/Script/Enemys/EnemyController.cs
+++ Assets/Script/Enemys/EnemyController.cs
@@ -22,7 +22,12 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] private float radiusSearch;
 
-   
+
+    public float maxHealth = 100;
+    [SerializeField] private float currentHealth;
+
+    private HealthBarEnemy healthBar;
+
 
     //es el punto donde el enemigo detecta al player
     private Vector3 startPointAttack;
@@ -40,16 +45,35 @@ public class EnemyController : MonoBehaviour
         Returning,
     }
 
+    
     void Start()
     {
+        currentHealth = maxHealth;
 
+        // instancia prefab de barra y lo inicializa
+        Canvas canvas = GameObject.Find("EnemyUIManager").GetComponent<Canvas>();
+        GameObject barObj = Instantiate(Resources.Load("HealthBarEnemy")) as GameObject;
+        barObj.transform.SetParent(canvas.transform, false);   // importante: false = conservar escala/pos
+        healthBar = barObj.GetComponent<HealthBarEnemy>();
+        healthBar.Setup(transform);
+        healthBar.SetHealth(currentHealth / maxHealth);
+        StartCoroutine(TestDamage());
     }
+
+    IEnumerator TestDamage()
+    {
+
+        yield return new WaitForSeconds(1);
+        TakeDamage(30);
+        Debug.Log("Tomo daño");
+    }
+   
 
     // Update is called once per frame
     void Update()
     {
         
-        
+
         switch (currentSate)
         {
             case MovementStates.Following:
@@ -63,7 +87,7 @@ public class EnemyController : MonoBehaviour
                 break;
         }
 
-       
+
 
     }
 
@@ -108,19 +132,20 @@ public class EnemyController : MonoBehaviour
 
     private void Follow()
     {
-        if (player == null) {
+        if (player == null)
+        {
             currentSate = MovementStates.Returning;
             return;
         }
         //Checka si esta a una distancia minima del player
         if (Vector3.Distance(transform.position, player.position) > minDistanceAtack && player != null && isInsideRange)
         {
-            
+
             //Se mueve al lugar del player
             transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
         //Verifica la distancia entre el jugador y el enemigo o el punto inicial del ataque y el enemigo, si se salen de rango de cualquiera de los 2 vuelve a patruyar 
-       if (Vector3.Distance(transform.position, player.position) > maxDistanceAggro || Vector3.Distance(transform.position, startPointAttack) > maxDistanceStartPointAttack)
+        if (Vector3.Distance(transform.position, player.position) > maxDistanceAggro || Vector3.Distance(transform.position, startPointAttack) > maxDistanceStartPointAttack)
         {
             currentSate = MovementStates.Returning;
             isInsideRange = false;
@@ -140,9 +165,9 @@ public class EnemyController : MonoBehaviour
     }
 
 
-     IEnumerator cooldownFollowing()
+    IEnumerator cooldownFollowing()
     {
-       
+
         yield return new WaitForSeconds(cooldownFollow);
         currentSate = MovementStates.Waiting;
     }
@@ -150,5 +175,18 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position, radiusSearch);
+    }
+    
+     public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(0, currentHealth);
+
+        healthBar.SetHealth(currentHealth / maxHealth);
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Script/Enemys/HealthBarEnemy.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarEnemy : MonoBehaviour
+{
+   [SerializeField] private Slider slider;
+   private Transform target;   // enemigo a seguir
+    private Vector3 offset;     // ajuste de posición (encima de la cabeza)
+
+    private Camera cam;
+
+
+    void Start()
+    {
+        cam = Camera.main;
+        offset = new Vector3(0, 1f, 0); 
+          // Busca el slider dentro del propio objeto (o de sus hijos)
+        slider = GetComponentInChildren<Slider>();
+        if (slider == null)
+        {
+        Debug.LogError("No se encontró un Slider en los hijos de la HealthBar", this);
+         }
+        
+    }
+
+    public void Setup(Transform targetTransform)
+    {
+        target = targetTransform;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+
+        // convertir posición del mundo a pantalla
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(target.position + offset);
+        transform.position = screenPos;
+        Debug.Log(transform.position);
+    }
+
+    public void SetHealth(float value)
+    {
+        slider.value = value;
+         Debug.Log($"Slider value set to: {value}");
+    }
+}
