d7870adcccaaf690fcda31655e191dbe17e33e88
FacundoMan <fmansillla@gmail.com>
2025-08-28 22:12:43 -0300

    Se agrego un escena nueva con "Boss" para empezar a testear habilidades

---
+++ Assets/Script/Enemys/Boss1/Pilon.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Pilon : MonoBehaviour
+{
+
+    [SerializeField] private int velMax;
+    [SerializeField] private int velMin;
+    [SerializeField] private Rigidbody pilarRb;
+
+    [SerializeField] private int velFin;
+    private void Start()
+    {
+        velFin = UnityEngine.Random.Range(velMin, velMax + 1);
+        pilarRb.linearVelocity = Vector3.up * velFin;
+        StartCoroutine(destruir());
+    }
+
+    IEnumerator destruir()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
+}
