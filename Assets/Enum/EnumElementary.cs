62abf39de586baf4d9bffa67c405b380fd2f4220
FacundoMan <fmansillla@gmail.com>
2025-09-09 17:21:10 -0300

    Se empezo a definir el SO que tendran los bosses

---
+++ Assets/Enum/EnumElementary.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public enum EnumElementry
+{
+    FIRE,
+    WATER,
+    AIR,
+    EARTH,
+    VOID,
+    LIGHT,
+    DARKNESS
+}
---
+++ Assets/SO/BossSO.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Boss", menuName = "Boss Data")]
+public class BossSO : ScriptableObject
+{
+    [SerializeField] private string nameBoss;
+    [SerializeField] private EnumElementry element;
+    [SerializeField] private int damage;
+    [SerializeField] private List<ScriptableObject> loot;
+    [SerializeField] private List<ScriptableObject> ability;
+    [SerializeField] private int maxHealth;
+    //Cantidad de armadura del boss
+    [SerializeField] private int armor;
+
+    [SerializeField] private string description;
+
+    public string NameBoss { get { return nameBoss; } }
+    public EnumElementry Element { get { return element; } }
+    public int Damage { get { return damage; } }
+    public List<ScriptableObject> Loot { get { return loot; } }
+    public List<ScriptableObject> Ability { get { return ability; } }
+
+    public int MaxHealth { get { return maxHealth; } }
+    
+    public string Description{get{ return description; }}
+    
+
+    
+}
